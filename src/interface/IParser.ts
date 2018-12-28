3310aeccb11b015dae26c2a340856d46d1b3682e
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-12-29 00:15:25 +0200

    add parser interface

---
+++ src/interface/IParser.ts
@@ -0,0 +1,8 @@
+import {IFixture} from './IFixture';
+
+export interface IParser {
+    priority: number;
+
+    isSupport(value: string): boolean;
+    parse(value: string, fixture: IFixture, entities: any): any;
+}
