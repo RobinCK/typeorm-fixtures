6a465210e4fbe57fcb91ed78bcac8f3e6f38981e
Igor Ognichenko <igor.ognichenko@fireblink.com>
2019-01-10 09:51:25 +0200

    test(resolver): resolved range fixtures

--- test/unit/Resolver.test.ts
+++ test/unit/Resolver.test.ts
@@ -40,4 +40,55 @@ describe('Resolver', () => {
             },
         ]);
     });
+
+    it('should be resolved range fixtures', () => {
+        const resolver = new Resolver();
+        const result = resolver.resolve([
+            {
+                entity: 'Post',
+                items: {
+                    'post{1..3}': {
+                        title: '{{name.title}}',
+                        description: '{{lorem.paragraphs}}',
+                    },
+                },
+            },
+        ]);
+
+        expect(result).to.deep.equal([
+            {
+                parameters: {},
+                entity: 'Post',
+                name: 'post1',
+                processor: undefined,
+                dependencies: [],
+                data: {
+                    title: '{{name.title}}',
+                    description: '{{lorem.paragraphs}}',
+                },
+            },
+            {
+                parameters: {},
+                entity: 'Post',
+                name: 'post2',
+                processor: undefined,
+                dependencies: [],
+                data: {
+                    title: '{{name.title}}',
+                    description: '{{lorem.paragraphs}}',
+                },
+            },
+            {
+                parameters: {},
+                entity: 'Post',
+                name: 'post3',
+                processor: undefined,
+                dependencies: [],
+                data: {
+                    title: '{{name.title}}',
+                    description: '{{lorem.paragraphs}}',
+                },
+            },
+        ]);
+    });
 });
