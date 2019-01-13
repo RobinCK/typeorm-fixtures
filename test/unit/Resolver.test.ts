5f140acf841bb3c8b1c157085a315dafa3bbaa43
Igor Ognichenko <ognichenko.igor@gmail.com>
2019-01-13 09:26:30 +0200

    test(resolver): resolve deep and mask

--- test/unit/Resolver.test.ts
+++ test/unit/Resolver.test.ts
@@ -122,4 +122,175 @@ describe('Resolver', () => {
 
         assert.isTrue(post.data.user === '@user2' || post.data.user === '@user1');
     });
+
+    it('should be resolved range fixtures', () => {
+        const resolver = new Resolver();
+        const result = resolver.resolve([
+            {
+                entity: 'Post',
+                items: {
+                    'post{1..3}': {
+                        title: '{{name.title}}',
+                        description: '{{lorem.paragraphs}}',
+                    },
+                },
+            },
+        ]);
+
+        expect(result).to.deep.equal([
+            {
+                parameters: {},
+                entity: 'Post',
+                name: 'post1',
+                processor: undefined,
+                dependencies: [],
+                data: {
+                    title: '{{name.title}}',
+                    description: '{{lorem.paragraphs}}',
+                },
+            },
+            {
+                parameters: {},
+                entity: 'Post',
+                name: 'post2',
+                processor: undefined,
+                dependencies: [],
+                data: {
+                    title: '{{name.title}}',
+                    description: '{{lorem.paragraphs}}',
+                },
+            },
+            {
+                parameters: {},
+                entity: 'Post',
+                name: 'post3',
+                processor: undefined,
+                dependencies: [],
+                data: {
+                    title: '{{name.title}}',
+                    description: '{{lorem.paragraphs}}',
+                },
+            },
+        ]);
+    });
+
+    it('should be resolved deep reference', () => {
+        const resolver = new Resolver();
+        const result = resolver.resolve([
+            {
+                entity: 'Post',
+                items: {
+                    post1: {
+                        title: '{{name.title}}',
+                        description: '{{lorem.paragraphs}}',
+                        data: {
+                            user: '@user1',
+                        },
+                    },
+                },
+            },
+            {
+                entity: 'User',
+                items: {
+                    user1: {
+                        firstName: '{{name.firstName}}',
+                        lastName: '{{name.lastName}}',
+                        email: '{{internet.email}}',
+                    },
+                },
+            },
+        ]);
+
+        const post: any = result.find(f => f.entity === 'Post') || { data: {} };
+
+        expect(post.dependencies).to.be.include('user1');
+    });
+
+    it('should be resolved mask reference', () => {
+        const resolver = new Resolver();
+        const result = resolver.resolve([
+            {
+                entity: 'Post',
+                items: {
+                    post1: {
+                        title: '{{name.title}}',
+                        description: '{{lorem.paragraphs}}',
+                        user: '@user*',
+                    },
+                },
+            },
+            {
+                entity: 'User',
+                items: {
+                    user1: {
+                        firstName: '{{name.firstName}}',
+                        lastName: '{{name.lastName}}',
+                        email: '{{internet.email}}',
+                    },
+                },
+            },
+        ]);
+
+        const post: any = result.find(f => f.entity === 'Post') || { data: {} };
+
+        expect(post.dependencies).to.be.include('user1');
+    });
+
+    it('should be fail resolved current', () => {
+        const resolver = new Resolver();
+
+        expect(() =>
+            resolver.resolve([
+                {
+                    entity: 'Post',
+                    items: {
+                        post: {
+                            title: '{{name.title}}',
+                            description: '{{lorem.paragraphs}}',
+                            user: '@user($current)',
+                        },
+                    },
+                },
+                {
+                    entity: 'User',
+                    items: {
+                        user1: {
+                            firstName: '{{name.firstName}}',
+                            lastName: '{{name.lastName}}',
+                            email: '{{internet.email}}',
+                        },
+                    },
+                },
+            ]),
+        ).to.be.throw('Error parsed index in reference: "user($current)" and fixture identify: post');
+    });
+
+    it('should be reference not found', () => {
+        const resolver = new Resolver();
+
+        expect(() =>
+            resolver.resolve([
+                {
+                    entity: 'Post',
+                    items: {
+                        post: {
+                            title: '{{name.title}}',
+                            description: '{{lorem.paragraphs}}',
+                            user: '@user2',
+                        },
+                    },
+                },
+                {
+                    entity: 'User',
+                    items: {
+                        user1: {
+                            firstName: '{{name.firstName}}',
+                            lastName: '{{name.lastName}}',
+                            email: '{{internet.email}}',
+                        },
+                    },
+                },
+            ]),
+        ).to.be.throw('Reference "user2" not found');
+    });
 });
