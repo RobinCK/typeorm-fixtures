e03cf8ff550c6e858bae0047139c02131d4d4ccc
Samuel Lukes <samuel.lukes@doctolib.com>
2023-09-26 08:02:26 +0200

    chore: improve performance of fixtures iterator

--- src/util/fixturesIterator.ts
+++ src/util/fixturesIterator.ts
@@ -1,25 +1,30 @@
-import { sum } from 'lodash';
 import { IFixture } from '../interface';
-
+import { PriorityQueue } from 'typescript-collections';
 export function* fixturesIterator(fixtures: IFixture[]) {
-    const state: any = {};
+    const processed = new Set<string>();
+    const queue = new PriorityQueue<IFixture>((a, b) => {
+        const aDependencies = a.dependencies.filter((dep) => !processed.has(dep)).length;
+        const bDependencies = b.dependencies.filter((dep) => !processed.has(dep)).length;
 
-    while (true) {
-        const result = fixtures.find((fixture) => {
-            return (
-                sum(
-                    fixture.dependencies.map((dependency: string) => {
-                        return state[dependency] === undefined ? 0 : 1;
-                    }),
-                ) === fixture.dependencies.length && !state[fixture.name]
-            );
-        });
+        return bDependencies - aDependencies;
+    });
 
-        if (result) {
-            state[result.name] = true;
-            yield result;
+    for (const fixture of fixtures) {
+        queue.add(fixture);
+    }
+
+    while (!queue.isEmpty()) {
+        const fixture = queue.dequeue();
+        if (!fixture) {
+            // theoretically impossible, but keep the linter happy.
+            continue;
+        }
+
+        if (fixture.dependencies.every((dep) => processed.has(dep))) {
+            processed.add(fixture.name);
+            yield fixture;
         } else {
-            return;
+            queue.add(fixture);
         }
     }
 }
--- test/unit/util/fixturesIterator.test.ts
+++ test/unit/util/fixturesIterator.test.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import { expect } from 'chai';
-import { fixturesIterator } from '../../../src/util';
+import { fixturesIterator, niaveFixturesIterator } from '../../../src/util';
 
 describe('Fixtures Iterator', () => {
     it('should be sort and iterate fixtures', () => {
@@ -48,5 +48,34 @@ describe('Fixtures Iterator', () => {
         expect(fixtures[0].name).to.equal('user1');
         expect(fixtures[1].name).to.equal('post1');
         expect(fixtures[2].name).to.equal('comment1');
+    });
+
+    it('should cope with 1 million fixtures in a reasonable period', () => {
+        const fixtures = [];
+
+        // Generate 1 million fixtures with random dependencies
+        for (let i = 0; i < 1_000_000; i++) {
+            const dependencies: string[] = [];
+            fixtures.push({
+                name: `fixture${i}`,
+                parameters: {},
+                entity: 'Comment',
+                dependencies,
+                data: {},
+            });
+        }
+
+        const start = Date.now();
+        const iterator = fixturesIterator(fixtures);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        for (const _fixture of iterator) {
+            // Do nothing
+        }
+        const end = Date.now();
+
+        // Check if the time taken is less than 2 seconds
+        // On macbook M1 this takes between 800 and 900ms
+        const timeTaken = end - start;
+        expect(timeTaken).to.be.lessThan(2000);
     });
 });
